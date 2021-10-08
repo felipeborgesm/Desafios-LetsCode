3520909f8bb7383ad294ea5c0b19b3e407af7aca
felipeborgesm <cafeborges@outlook.com>
2021-10-08 17:41:04 -0300

    condição para caso coloquem dois operadores

--- Calculadora.js
+++ Calculadora.js
@@ -22,7 +22,10 @@ let calculadora = (() => {
 
    const equals = () => {
         let calculo; 
-        if (numeroHistorico.length > 0 && operacaoHistorico.length > 0) {
+        if (numeroHistorico.length % 2 !== 0) {// caso coloquem duas operações ao invés de dois números, o calculo resultará em null
+            numeroHistorico.push(0);
+            calculo = null;
+        } else {
             if (ultitimoTermo(operacaoHistorico) === '+') {
                 calculo = penultitimoTermo(numeroHistorico) + ultitimoTermo(numeroHistorico); 
             }
@@ -94,5 +97,15 @@ calculadora.reset();
 calculadora.enter(10);
 calculadora.enter('-');
 calculadora.enter(5.4);
+calculadora.equals();
+calculadora.list();
+calculadora.enter(10);
+calculadora.enter('-');
+calculadora.enter('+');
+calculadora.equals();
+calculadora.list();
+calculadora.enter(4);
+calculadora.enter('*');
+calculadora.enter(3);
 calculadora.equals();
 calculadora.list();
